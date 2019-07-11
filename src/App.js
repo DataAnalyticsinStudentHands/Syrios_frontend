3a24370c49ac7599ea9c54c607780c07d30a23d6
Anjana0987 <anjana.rajasekhar1228@gmail.com>
2019-07-10 22:13:49 -0500

    10/07

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import logo from './img_2.png';
+import Collection from './components/Collection'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1 className="App-h1">Finding Connections in Ancient Syria</h1>
-      <table className='App-t1' cellSpacing= '1'>
-        <tr>
-          <td>
-          <img src={logo} className="App-logo" alt="logo" />
-          </td>
-          <td>
-            <tr>
-            <button className = 'App-b1' type = "button"> Explore the collection </button>
-            </tr>
-            <tr>
-            <button className = 'App-b1' type = "button">Follow The Coins</button>
-            </tr>
-            <tr>
-            <button className = 'App-b1' type = "button">Discover the Story</button>
-            </tr>
-            <tr>
-            <button className = 'App-b1' type = "button">Learn About the Coins</button>
-            </tr>
-            <tr>
-            <button className = 'App-b1' type = "button">Meet the Team</button>
-            </tr>
-          </td>
-        </tr>
-      </table>
-    
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+        <h1 className="App-h1">Finding Connections in Ancient Syria</h1>
+        <table className='App-t1' cellSpacing= '1'>
+          <tr>
+            <td>
+            <img src={logo} className="App-logo" alt="logo" />
+            </td>
+            <td>
+              <tr>
+              <button className = 'App-b1'> 
+              <Link to="/collections" className = 'words'> Explore the Collection </Link>
+              </button>
+              </tr>
+              <tr>
+              <button className = 'App-b1' type = "button" >Follow The Coins</button>
+              </tr>
+              <tr>
+              <button className = 'App-b1' type = "button">Discover the Story</button>
+              </tr>
+              <tr>
+              <button className = 'App-b1' type = "button">Learn About the Coins</button>
+              </tr>
+              <tr>
+              <button className = 'App-b1' type = "button">Meet the Team</button>
+              </tr>
+            </td>
+          </tr>
+        </table>
+      
+        </header>
+      </div>
+      <Route exact path ="/collections" component={Collection} />
+    </Router>
   );
 }
 
