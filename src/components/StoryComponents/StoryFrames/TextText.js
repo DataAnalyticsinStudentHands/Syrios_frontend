7c346eef1e3273f455d59294409aee7b85e87844
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2022-06-23 17:37:16 -0500

    Small update on Frames

--- src/components/StoryComponents/StoryFrames/TextText.js
+++ src/components/StoryComponents/StoryFrames/TextText.js
@@ -4,6 +4,38 @@ import { HeadComponent, TextComponent } from "../ComponentFunction/index";
 
 const TextText = (props) => {
   let zone = props.zone
+  const TextTxtBody = ()=>{
+    if ((zone.tt_text_left.text === '' || zone.tt_text_left.text === null) && (zone.tt_text_left.caption === '' || zone.tt_text_left.caption === null)){
+      return(
+        <Row className="d-flex justify-content-around align-items-center">
+          <Col xs={6}>
+            <TextComponent text = {zone.tt_text_right}/>
+          </Col>
+        </Row>
+      )
+    }
+    else if((zone.tt_text_right.text === '' || zone.tt_text_right.text === null) && (zone.tt_text_right.caption === '' || zone.tt_text_right.caption === null)){
+      return(
+        <Row className="d-flex justify-content-around align-items-center">
+          <Col xs={6}>
+            <TextComponent text = {zone.tt_text_left}/>
+          </Col>
+        </Row>
+      )
+    }
+    else{
+      return(
+        <Row className="d-flex justify-content-around align-items-center">
+          <Col xs={5}>
+            <TextComponent text = {zone.tt_text_left}/>
+          </Col>
+          <Col xs={5}>
+            <TextComponent text = {zone.tt_text_right}/>
+          </Col>
+        </Row>
+      )
+    }
+  }
     if (zone.head.updown_switch){
       return(
         <div 
@@ -15,14 +47,15 @@ const TextText = (props) => {
           }}
         >
         <Container>
-          <Row className="d-flex justify-content-around align-items-center mb-5">
+          {/* <Row className="d-flex justify-content-around align-items-center mb-5">
             <Col xs={5}>
               <TextComponent text = {zone.tt_text_left}/>
             </Col>
             <Col xs={5}>
               <TextComponent text = {zone.tt_text_right}/>
             </Col>
-          </Row>
+          </Row> */}
+          <TextTxtBody/>
           <Row className="mt-5">
             <HeadComponent storyMain = {zone.head.head_main} storyCaption = {zone.head.head_caption}/>
           </Row>
@@ -44,14 +77,15 @@ const TextText = (props) => {
           <Row className="mb-5">
             <HeadComponent storyMain = {zone.head.head_main} storyCaption = {zone.head.head_caption}/>
           </Row>
-          <Row className="d-flex justify-content-around align-items-center mt-5">
+          {/* <Row className="d-flex justify-content-around align-items-center mt-5">
             <Col xs={5}>
               <TextComponent text = {zone.tt_text_left}/>
             </Col>
             <Col xs={5}>
               <TextComponent text = {zone.tt_text_right}/>
             </Col>
-          </Row>
+          </Row> */}
+          <TextTxtBody/>
         </Container>
       </div>
       )
