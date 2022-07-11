5c2448cead6f098c43d0531e8a83ef6276068c51
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2022-07-11 10:07:37 -0500

    Update  Event popup

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,15 +1,35 @@
 export const colors = {
   roman: '#B85828',
-  greek: '#F2D16B',
+  persina:'#78a878',
+  greek: '#Fd8b848',
+  syrios:'#737271',
+  political:'#486678',
+  military:'#313029',
+  cultural:'d88858',
+  economic:'#d8a048',
   eastern_states: '#7FA87F',
   default: '#71797E',
   find_color: function(text) {
     text = text.toLowerCase();
     if (text.includes('roman'))
       return this.roman;
+    if (text.includes('persina'))
+      return this.persina;
     if (text.includes('greek'))
       return this.greek;
-    if (text.includes('eastern') || text.includes('persian'))
+    if (text.includes('syrios'))
+      return this.syrios;
+    if (text.includes('political'))
+      return this.political;
+    if (text.includes('military'))
+      return this.military;
+    if (text.includes('cultural'))
+      return this.cultural;
+    if (text.includes('economic'))
+      return this.economic;
+    if (text.includes('economic'))
+      return this.economic;
+    if (text.includes('eastern'))
       return this.eastern_states;
     return this.default;
   }
