9e76a0b37536cc9be3905ae3df30e51120e630db
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2022-07-14 14:04:44 -0500

    Fix the warning on NavBar, add icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import {
   NavDropdown,
   Form,
 } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 import logo from 'src/assets/logoWhiteText.svg';
 
@@ -13,7 +13,7 @@ function BootstrapNavbar() {
   return (
     <Navbar id='navbar' collapseOnSelect expand='md' className='navbar-dark'>
       <Navbar.Brand>
-        <Link to='/'>
+        <Nav.Link href='/dev'>
           <img 
             src={logo} 
             alt='SyriosLogoLight'
@@ -22,29 +22,29 @@ function BootstrapNavbar() {
               position: 'relative',
               left: '10px'
             }}/>
-        </Link>
+        </Nav.Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls='responsive-navbar-nav' />
       <Navbar.Collapse id='responsive-navbar-nav'>
         <Nav className='ms-auto navbar-text'>
           { /* I hate this, but I can't figure out why Nav.Link won't work */ }
           {/* Because Nav is from Bootstrap and Link is from react!!!!!!! */}
-            <Nav.Item><Link to='/Stories' className='navbar-text'style={{position: 'relative',top: '8px',left: '-20px'}}>STORIES</Link></Nav.Item>
+            <Nav.Link href='/dev/Stories' className='navbar-text'>STORIES</Nav.Link>
           <NavDropdown title='EVIDENCE' className='NavDropdown'>
-            <NavDropdown.Item><Link to ="/Evidence/CoinSort" className='navbar-text'>Coins in a Pile</Link></NavDropdown.Item>
-            <NavDropdown.Item><Link to='/Evidence/MapCoins' className='navbar-text'>Coins on a Map</Link></NavDropdown.Item>
-            <NavDropdown.Item><Link to='/Evidence/CoinCatalogy' className='navbar-text'>Coins in a Catalog</Link></NavDropdown.Item>
-            <NavDropdown.Item><Link to='/Evidence/Download' className='navbar-text'>Coins as Data</Link></NavDropdown.Item>
+            <NavDropdown.Item href='/dev/Evidence/CoinSort' className='navbar-text'>Coins in a Pile</NavDropdown.Item>
+            <NavDropdown.Item href='/dev/Evidence/MapCoins' className='navbar-text'>Coins on a Map</NavDropdown.Item>
+            <NavDropdown.Item href='/dev/Evidence/CoinCatalogy' className='navbar-text'>Coins in a Catalog</NavDropdown.Item>
+            <NavDropdown.Item href='/dev/Evidence/Download' className='navbar-text'>Coins as Data</NavDropdown.Item>
           </NavDropdown>
           <NavDropdown title='TOOL BOX' className='NavDropdown'>			
-            <NavDropdown.Item><Link to='/HowToReadACoin' className='navbar-text'>How to Read a coin</Link></NavDropdown.Item>
-            <NavDropdown.Item><Link to='/Toolbox/VideoLibrary' className='navbar-text'>Video library</Link></NavDropdown.Item>
-            <NavDropdown.Item><Link to='/Toolbox/Timeline' className='navbar-text'>Timeline</Link></NavDropdown.Item>
-            <NavDropdown.Item><Link to='/Toolbox/Glossary/all' className='navbar-text'>Glossary</Link></NavDropdown.Item>
+            <NavDropdown.Item href='/dev/HowToReadACoin' className='navbar-text'>How to Read a coin</NavDropdown.Item>
+            <NavDropdown.Item href='/dev/Toolbox/VideoLibrary' className='navbar-text'>Video library</NavDropdown.Item>
+            <NavDropdown.Item href='/dev/Toolbox/Timeline' className='navbar-text'>Timeline</NavDropdown.Item>
+            <NavDropdown.Item href='/dev/Toolbox/Glossary/all' className='navbar-text'>Glossary</NavDropdown.Item>
           </NavDropdown>
           <NavDropdown title='RESOURCES' className='NavDropdown'>							
-            <NavDropdown.Item><Link to='/Resources/TeachingResources' className='navbar-text'>Teaching Resources</Link></NavDropdown.Item>
-            <NavDropdown.Item><Link to='/Resources/Research' className='navbar-text'>Research</Link></NavDropdown.Item>
+            <NavDropdown.Item href='/dev/Resources/TeachingResources' className='navbar-text'>Teaching Resources</NavDropdown.Item>
+            <NavDropdown.Item href='/dev/Resources/Research' className='navbar-text'>Research</NavDropdown.Item>
           </NavDropdown>
         </Nav>
         <Form style={{width: '145px', marginRight: '10px'}}>
