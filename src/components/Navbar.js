7b8c6267f940223abb3e5a7f8590fa0647076a60
peggylind <peggylind@gmail.com>
2022-08-12 19:35:31 -0500

    added overview links to navbar using now BrowserRouter Link in navbar instead of href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,9 @@ import {
   NavDropdown,
 } from 'react-bootstrap';
 
+import {
+  Link
+} from "react-router-dom";
 import logo from 'src/assets/logoWhiteText.svg';
 
 
@@ -27,23 +30,28 @@ function BootstrapNavbar() {
         <Nav className='ms-auto'  style={{marginRight:'5.2vmax'}}>
           { /* I hate this, but I can't figure out why Nav.Link won't work */ }
           {/* Because Nav is from Bootstrap and Link is from react!!!!!!! */}
-            <Nav.Link href='/dev' className='navbar-text d-flex align-items-center'>HOME</Nav.Link>
-            <Nav.Link href='/dev/Stories' className='navbar-text d-flex align-items-center'>STORIES</Nav.Link>
+            <Nav.Link as={Link} to='/' className='navbar-text d-flex align-items-center'>HOME</Nav.Link>
+            {/* <Nav.Link href='/dev/Stories' className='navbar-text d-flex align-items-center'>STORIES</Nav.Link> */}
+            <Nav.Link as={Link}  to='/Stories' className='navbar-text d-flex align-items-center'>STORIES</Nav.Link>
           <NavDropdown title='EVIDENCE' className='navbar-text'>
-            <NavDropdown.Item href='/dev/Evidence/CoinSort' className='navbar-text '>Coins in a Pile</NavDropdown.Item>
-            <NavDropdown.Item href='/dev/Evidence/MapCoins' className='navbar-text'>Coins on a Map</NavDropdown.Item>
-            <NavDropdown.Item href='/dev/Evidence/Timeline' className='navbar-text'>Coins in Time</NavDropdown.Item>
-            <NavDropdown.Item href='/dev/Evidence/CoinCatalog' className='navbar-text'>Coins in a Catalog</NavDropdown.Item>
-            <NavDropdown.Item href='/dev/Evidence/Download' className='navbar-text'>Coins as Data</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to='/Evidence' className='navbar-text '>Overview</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item as={Link} to='/Evidence/CoinSort' className='navbar-text '>Coins in a Pile</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to='/Evidence/MapCoins' className='navbar-text'>Coins on a Map</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to='/Evidence/Timeline' className='navbar-text'>Coins in Time</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to='/Evidence/CoinCatalog' className='navbar-text'>Coins in a Catalog</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to='/Evidence/Download' className='navbar-text'>Coins as Data</NavDropdown.Item>
           </NavDropdown>
-          <NavDropdown title='TOOL BOX' className='navbar-text'>			
-            <NavDropdown.Item href='/dev/HowToReadACoin' className='navbar-text'>How to Read a coin</NavDropdown.Item>
-            <NavDropdown.Item href='/dev/Toolbox/VideoLibrary' className='navbar-text'>Video library</NavDropdown.Item>
-            <NavDropdown.Item href='/dev/Toolbox/Glossary/all' className='navbar-text'>Glossary</NavDropdown.Item>
+          <NavDropdown title='TOOL BOX' className='navbar-text'>
+            <NavDropdown.Item as={Link} to='/Toolbox' className='navbar-text '>Overview</NavDropdown.Item>
+            <NavDropdown.Divider />			
+            <NavDropdown.Item as={Link} to='/HowToReadACoin' className='navbar-text'>How to Read a coin</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to='/Toolbox/VideoLibrary' className='navbar-text'>Video library</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to='/Toolbox/Glossary/all' className='navbar-text'>Glossary</NavDropdown.Item>
           </NavDropdown>
           <NavDropdown title='RESOURCES' className='navbar-text'>							
-            <NavDropdown.Item href='/dev/Resources/TeachingResources' className='navbar-text'>Teaching Resources</NavDropdown.Item>
-            <NavDropdown.Item href='/dev/Resources/Research' className='navbar-text'>Research</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to='/Resources/TeachingResources' className='navbar-text'>Teaching Resources</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to='/Resources/Research' className='navbar-text'>Research</NavDropdown.Item>
           </NavDropdown>
         </Nav>
         {/* <Form style={{width: '145px', marginRight: '10px'}}>
