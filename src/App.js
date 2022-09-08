3a2b1124ea5649cd93c499d6a67033f2b2f0a2d2
peggylind <peggylind@gmail.com>
2022-09-08 09:03:12 -0500

    setup main for production (no /dev and proper .htaccess)

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
 			style={{ backgroundImage: `url(${background})`}}
 		>
       { /* Change line below: <BrowserRouter basename='/dev'> to deploy on syrios.uh.edu/dev */ }
-			<BrowserRouter basename='/dev'>
+			<BrowserRouter>
 				<Navbar />
 				<Routes>
 					<Route exact path='/' element={<LandingPage />} />
