aedec6aff8aa29151769b8f0b3facdc4c4e3125c
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2022-09-22 17:36:32 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
 
 		<div id='App' style={{ backgroundImage: `url(${background})`}}>
       { /* Change line below: <BrowserRouter basename='/dev'> to deploy on syrios.uh.edu/dev */ }
-			<BrowserRouter>
+			<BrowserRouter basename='/dev'>
 				<Navbar />
 				<Routes>
 					<Route exact path='/' element={<LandingPage />} />
