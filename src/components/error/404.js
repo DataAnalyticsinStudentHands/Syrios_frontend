5782ac70410b6fb38cc0702aba3039f5dcc28c70
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2022-10-14 15:10:07 -0500

    Testing content with the New footer

--- src/components/error/404.js
+++ src/components/error/404.js
@@ -1,10 +1,6 @@
 import React from 'react';
-
-// import Footer from 'src/components/footer/Footer';
-import Footer2 from '../footerv2/Footer2';
 const ErrorPage = ()=>{
     return(
-        <>
         <div id='error-page'>
             <center>
                 <h1>404</h1>
@@ -12,9 +8,6 @@ const ErrorPage = ()=>{
             </center>
 
         </div>
-        <Footer2/>
-        </>
-
     )
 }
 export default ErrorPage
--- src/pages/Stories/Stories.js
+++ src/pages/Stories/Stories.js
@@ -34,7 +34,6 @@ const Stories = () => {
 	if (isLoading)return (<><LoadingPage /><Footer /></>);
 	return (
 		<div id='stories-page'>
-			<div style={{width:'80%', marginLeft:'10%'}}>
                 <PageTitleComponent title={storyContent.head} text={storyContent.text} subtext={storyContent.sub_text}/>
 				<Row className="py-5 my-5 align-items-end ">
 					{stories.map((story)=>{
@@ -50,7 +49,6 @@ const Stories = () => {
 						)
 					})}
 				</Row>
-			</div>
 		</div>
 	)
 }
