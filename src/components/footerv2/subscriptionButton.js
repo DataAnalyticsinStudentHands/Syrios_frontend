e6b64f272fd718b4b276c30ceadaa9910fde0b22
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2022-11-18 09:11:21 -0600

    update Subscription form

--- src/components/footerv2/subscriptionButton.js
+++ src/components/footerv2/subscriptionButton.js
@@ -1,50 +1,36 @@
-import React, {useState} from "react";
-import { useFormik} from 'formik';
-import * as Yup from "yup"
+import React, {useState} from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 import axios from 'axios';
 
+const SubscriptionSchema = yup.object().shape({
+	email: yup.string().email('*Must be a valid email address').max(100, '*Email must be less than 100 characters')
+})
 function SubscriptionButton(){
-
     const [show, setShow] = useState(false)
-
-    const formik = useFormik({
-        initialValues:{
-          email:""
-        },
-        validationSchema:Yup.object().shape({
-            email: Yup.string()
-              .email('*Must be a valid email address')
-              .max(100, '*Email must be less than 100 characters')
-              .required('* Email is required')
-          }),
-        onSubmit: (values,{resetForm})=>{
-            axios.post(`${process.env.REACT_APP_strapiURL}/api/user-subscription`, {data:values})
-            .then(resetForm())
-            .then(setShow(true))
-            .catch(err =>{console.error(err)})
-        }
-      })
-
+	const {register, handleSubmit, formState: { errors }} = useForm({
+		resolver: yupResolver(SubscriptionSchema)
+	})
+	const onSubmit = (data) => {
+        axios.post(`${process.env.REACT_APP_strapiURL}/api/user-subscription`, {data:data})
+        .then(setShow(true))
+        .catch(err =>{console.error(err)})
+    }
     return(
-        <div>
-            {show
-                ? <div id="footer2-syrios"> Thanks for follow us!</div>
-                :<form className="input-addon" onSubmit={formik.handleSubmit}>
-                    <input type="text" name="email" id="footer2-email" placeholder="Enter your email" className="input-addon__input" 
-                        onChange={formik.handleChange}
-                        onBlur = {formik.handleBlur}
-                        value = {formik.values.email}
-                    />
-                    <div className="input-addon__addon input-addon__addon--appended">
-                        <button  type="submit" id="footer_submit" className="px-3">Submit</button>
-                    </div>
-                </form>
+        <>
+            {show 
+            ? <div id="footer2-syrios"> Thanks for follow us!</div>
+            :<form className="input-addon" onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" name="email" id="footer2-email" placeholder="Enter your email" className="input-addon__input"
+                    {...register("email")}/>
+                <div className="input-addon__addon input-addon__addon--appended">
+                    <button  type="submit" id="footer_submit" className="px-3">Submit</button>
+                </div>
+            </form>
             }
-            {formik.touched.email && formik.errors.email ? <div className="error-message">{formik.errors.email}</div>: null}
-
-        </div>
-
+            {errors.email && <div className="error-message">{errors.email.message}</div>}
+        </>
     )
-
 }
 export default SubscriptionButton
